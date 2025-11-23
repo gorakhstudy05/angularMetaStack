618df265fb0c070d89846480c4d112a23ca8814e
gorakhstudy05 <gorakhstudy05@gmail.com>
2025-11-23 18:11:36 +0530

    feat: implement authentication and product features with routing and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('../app/features/auth/Auth/auth-guard')
+      .then(m => m.AUTH_ROUTES)
+  }
+];
